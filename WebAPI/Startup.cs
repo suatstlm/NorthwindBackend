831bf67b08b1af8d05026b385cb061f43bf06a62
SUAT SATILMIŞ <suat.satilmis@ym.com.tr>
2022-10-19 13:27:19 +0300

    Added JWT Authorization for swagger UI

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,9 +30,26 @@ namespace WebApı
             services.AddControllers();
             services.AddCors();
 
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(opt =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApı", Version = "v1" });
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApı", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345.54321\""
+                });
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                             { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" } },
+                        new string[] { }
+                    }
+                });
             });
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
